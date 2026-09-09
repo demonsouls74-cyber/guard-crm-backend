521d85ea7c284ba98813f00d438bd4ec233165e3
demonsouls74-cyber <demonsouls74@gmail.com>
2026-09-09 23:21:04 +0300

    on the token verify

--- backend/main.py
+++ backend/main.py
@@ -133,7 +133,7 @@ def read_root():
 
 # Наш новий закритий ендпоінт для створення користувачів
 @app.post("/users/")
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)): 
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db), token_data: dict = Depends(allow_admin)): 
     # 1. Перевіряємо, чи немає вже когось із таким email
     db_user = db.query(models.User).filter(models.User.email == user.email).first()
     if db_user:
